da54f3b0e81649fbef825fa5f69feae4c0ea46ab
Olamilekan12 <olamilekanogunbade3@gmail.com>
2024-08-21 18:49:48 -0700

    adding necessary features and functions

--- script.js
+++ script.js
@@ -9,63 +9,96 @@ function dropDownMenu() {
   }
 }
 
-let speech = new SpeechSynthesisUtterance();
+// // Web Speech API
 
-let voices = [];
 
-let voiceSelect = document.querySelector("select");
+// text to speech
 
-window.speechSynthesis.onvoiceschanged = () => {
-  voices = window.speechSynthesis.getVoices();
-  speech.voice = voices[0];
+const textarea = document.getElementById("text"),
+voiceList = document.querySelector("select"),
+speechBtn = document.getElementById("play-button");
 
-  voices.forEach(
-    (voice, i) => (voiceSelect.options[i] = new Option(voice.name, i))
-  );
-};
+let synth = speechSynthesis,
+isSpeaking = true;
 
-voiceSelect.addEventListener("change", () => {
-  speech.voice = voices[voiceSelect.value];
+voices();
+
+function voices(){
+    for(let voice of synth.getVoices()){
+        let selected = voice.name === "Google US English" ? "selected" : "";
+        let option = `<option value="${voice.name}" ${selected}>${voice.name} (${voice.lang})</option>`;
+        voiceList.insertAdjacentHTML("beforeend", option);
+    }
+}
+
+synth.addEventListener("voiceschanged", voices);
+
+function textToSpeech(text){
+    let utterance = new SpeechSynthesisUtterance(text);
+    for(let voice of synth.getVoices()){
+        if(voice.name === voiceList.value){
+            utterance.voice = voice;
+        }
+    }
+    synth.speak(utterance);
+}
+
+speechBtn.addEventListener("click", e =>{
+    e.preventDefault();
+    if(textarea.value !== ""){
+        if(!synth.speaking){
+            textToSpeech(textarea.value);
+        }
+        if(textarea.value.length > 80){
+            setInterval(()=>{
+                if(!synth.speaking && !isSpeaking){
+                    isSpeaking = true;
+                    speechBtn.innerText = "Convert To Speech";
+                }else{
+                }
+            }, 500);
+            if(isSpeaking){
+                synth.resume();
+                isSpeaking = false;
+                speechBtn.innerText = "Pause Speech";
+            }else{
+                synth.pause();
+                isSpeaking = true;
+                speechBtn.innerText = "Resume Speech";
+            }
+        }else{
+            speechBtn.innerText = "Convert To Speech";
+        }
+    }
 });
 
-document.querySelector("button").addEventListener("click", () => {
-  speech.text = document.querySelector("textarea").value;
-  window.speechSynthesis.speak(speech);
+
+
+
+
+// Speech/Voice To Text
+
+var convert_text= document.getElementById('convert_text');
+var click_to_convert= document.getElementById('click_to_convert');
+
+click_to_convert.addEventListener('click', function(){
+    var speech = true;
+   
+    window.SpeechRecognition = window.webkitSpeechRecognition;
+    const recognition = new SpeechRecognition();
+    recognition.interimResults=true;
+recognition.maxResults= 100;
+console.log(speech);
+    recognition.addEventListener('result', e => {
+        const transcript = Array.from(e.results)
+        .map(result =>result[0])
+        .map(result => result.transcript)
+        convert_text.innerHTML = transcript;
+    })
+
+    if(speech == true){
+        recognition.start();
+    }
 });
 
-//code to switch themes between dark mode and light mode
 
-document.getElementById("swap").addEventListener("click", function () {
-  document.body.classList.toggle("light-mode");
-
-  if (document.body.classList.contains("light-mode")) {
-    localStorage.setItem("swap", "light");
-  } else {
-    localStorage.setItem("swap", "dark");
-  }
-});
-
-//load the saved theme preference on page load
-window.onload = function () {
-  if (localStorage.getItem("swap") === "light") {
-    document.body.classList.add("light-mode");
-  }
-};
-
-// click_to_convert.addEventListener('click', function(){
-//     var speech = true;
-//     window.SpeechRecognition = window.webkitSpeechRecognition;
-//     const recognition = new SpeechRecognition();
-//     recognition.interimResults=true;
-
-//     recognition.addEventListener('result', e=>{
-//         const transcript = Array.from(e.results)
-//         .map(result =>result[0])
-//         .map(result => result.transcript)
-//         convert_text.innerHTML = transcript;
-//     })
-
-//     if(speech == true){
-//         recognition.start();
-//     }
-// })
